6f9cd94e70c436d65a13d7d2dccc813e6795a1ce
Benjamen Alford <benjamen.alford@gmail.com>
2023-10-31 20:24:23 -0500

    showing table

--- generic_flask/static/js/app.js
+++ generic_flask/static/js/app.js
@@ -1,9 +1,28 @@
-console.log("i am app");
+let songTable = d3.select("#songs")
+let songTableBody = songTable.append("tbody");
 
 d3.json("/api/songs").then(data => {
-    console.log('this is data loaded from the api. ');
-    console.log(data);
+    // console.log(data);
+    //create the header
+    let headerRow = d3.select("#songs").select('thead').append('tr')
+    Object.keys(data[0]).forEach(key => {
+        if (!key.startsWith("_id"))
+            headerRow.append('th').text(key)
+    })
+
     data.forEach(element => {
-        console.log(element)
+        // console.log(element)
+        let newRow = songTableBody.append("tr")
+        Object.entries(element).forEach(([key, value]) => {
+            console.log(key, value)
+            if (!key.startsWith("_id")) {
+
+                if (key == "url") {
+                    newRow.append('td').append('a').text(value).attr('href', value).attr('target', '_blank')
+                } else {
+                    newRow.append('td').text(value)
+                }
+            }
+        })
     });
 })
